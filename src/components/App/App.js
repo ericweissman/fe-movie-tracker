3b8fa3134808b5a8e3888ee77abd6caecafad6f3
Hillary Stewart <hillarystewart4427@gmail.com>
2019-01-29 15:47:48 -0700

    Add Redux boilerplate

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        
       </div>
     );
   }
