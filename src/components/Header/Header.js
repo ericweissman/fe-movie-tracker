82fb2808dba8e6c1b498c80273c5e2d9a67cbfa1
Hillary Stewart <hillarystewart4427@gmail.com>
2019-02-04 17:32:37 -0700

    Add spacing to header links and logout icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,39 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 export const Header = ({ logoutUser, user }) => {
   return (
-    <header className='header'>
+    <header className="header">
       <h1>Movie Tracker</h1>
       <nav>
-        <NavLink to='/' className='nav'>Show All Movies</NavLink>
-        {!user.id && <NavLink to='/login' className='nav'>Login</NavLink>}
-        {!user.id && <NavLink to='/signup' className='nav'>Signup</NavLink>}
-        {!user.id && <NavLink to='/login' className='nav'>View Favorites</NavLink>}
-        {user.id && <NavLink to='/favorites' className='nav'>View Favorites</NavLink>}
-        {user.id && <button onClick={logoutUser}>LOG OUT</button>}
+        <NavLink to="/" className="nav">
+          Show All Movies
+        </NavLink>
+        {!user.id && (
+          <NavLink to="/login" className="nav">
+            Login
+          </NavLink>
+        )}
+        {!user.id && (
+          <NavLink to="/signup" className="nav">
+            Signup
+          </NavLink>
+        )}
+        {!user.id && (
+          <NavLink to="/login" className="nav">
+            View Favorites
+          </NavLink>
+        )}
+        {user.id && (
+          <NavLink to="/favorites" className="nav">
+            View Favorites
+          </NavLink>
+        )}
+        {user.id && (
+          <Link to="/">
+            <button onClick={logoutUser} />
+          </Link>
+        )}
       </nav>
     </header>
   )
